3815bea5f2411904b83754ed221ed3428f7e9648
Samarth-Bhat <93364100+Samarth-Bhat@users.noreply.github.com>
2024-09-10 23:11:34 +0530

    Update DBConnect.java

--- src/main/java/com/conn/DBConnect.java
+++ src/main/java/com/conn/DBConnect.java
@@ -3,27 +3,29 @@ package com.conn;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class DBConnect 
-{
+public class DBConnect {
 
-	private static Connection conn = null;
-	
-	public static Connection getConn()
-	{
-		try {
-			
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		//	conn = DriverManager.getConnection("jdbc:sqlite:mydatabase.db");
-			conn = DriverManager.getConnection("jdbc:sqlite:C:/Users/Swapnil/eclipse-workspace/Online Electronic Shopping/mydatabase.db");
-			
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-		
-		return conn;
-	}
-	
+    private static Connection conn = null;
+    private static DBConnect instance = null;
+
+    private DBConnect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:sqlite:C:/Users/Swapnil/eclipse-workspace/Online Electronic Shopping/mydatabase.db");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized DBConnect getInstance() {
+        if (instance == null) {
+            instance = new DBConnect();
+        }
+        return instance;
+    }
+
+    // Method to get the database connection
+    public Connection getConn() {
+        return conn;
+    }
 }
